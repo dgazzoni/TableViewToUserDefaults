93db814fc9113f65decb102647cc5440269c283c
Décio Luiz Gazzoni Filho <decio@decpp.net>
2019-01-27 00:20:10 -0200

    Initial Commit

---
+++ TableViewToUserDefaults/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableViewToUserDefaults
+//
+//  Created by Décio Luiz Gazzoni Filho on 27/01/19.
+//  Copyright © 2019 Décio Luiz Gazzoni Filho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TableViewToUserDefaults/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableViewToUserDefaults
+//
+//  Created by Décio Luiz Gazzoni Filho on 27/01/19.
+//  Copyright © 2019 Décio Luiz Gazzoni Filho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
